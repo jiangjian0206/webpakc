be21cf75c771a7dfe9f1020e68d2291210432329
jiangjian0206 <756028992@qq.com>
2018-11-25 15:14:41 +0800

    change the printf

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack config.js'], ' ');
+    element.innerHTML = _.join(['Hello this is git branch', 'webpack config.js'], ' ');
   
     return element;
   }
